3488bb83da2992854125d2db6cf6dca84f2c59c9
mishrayash01 <yashmishra1102005@gmail.com>
2026-02-06 15:18:02 +0530

    Fix Env type definition

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,8 @@
 import { Hono } from "hono";
 
+// This line fixes the error by telling TypeScript what "Env" is
+type Env = Record<string, any>;
+
 const app = new Hono<{ Bindings: Env }>();
 
 export default app;
